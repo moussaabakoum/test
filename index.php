d6f947a099890cee0d7b58d57bf6c716e8c32372
moussaabakoum <49553334+moussaabakoum@users.noreply.github.com>
2022-12-08 16:20:42 +0200

    Update index.php
    added var dump

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-foreach ($_POST as $param_name => $param_val) {
-    echo "Param: $param_name; Value: $param_val<br />\n";
-}
+var_dump($_POST);
+// foreach ($_POST as $param_name => $param_val) {
+//     echo "Param: $param_name; Value: $param_val<br />\n";
+// }
 
